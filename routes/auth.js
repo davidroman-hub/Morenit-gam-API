ad3b7b5f60b95338a8149c471335a2f4f70fca69
davidroman-hub <jobroman83@gmail.com>
2020-05-16 02:30:35 +0100

    sign up success

---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+// import controller
+
+const {signup} = require('../controllers/auth')
+
+// import validators
+
+const {userSignupValidator,userSigninValidator} = require('../validators/auth')
+const {runValidation} = require('../validators/index')
+
+
+router.post('/signup', userSignupValidator, runValidation, signup)
+
+
+
+module.exports = router // {}
