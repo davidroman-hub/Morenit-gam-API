2722b304a0bca4883a4df13064c935e5311fe0d8
davidroman-hub <jobroman83@gmail.com>
2020-05-23 00:23:16 +0100

    generate blog with tags, and categories arrays, and with the validations

--- controllers/blog.js
+++ controllers/blog.js
@@ -19,6 +19,30 @@ exports.create = (req, res) => {
         }
         const {title,body, categories, tags} = fields
 
+        if(!title || !title.length){
+            return res.status(400).json({
+                error:'El titulo es Requerido'
+            })
+        }
+        
+        if(!body || body.length < 200 ){
+            return res.status(400).json({
+                error:'El contenido es muy corto'
+            })
+        }
+        
+        if(!categories || categories.length == 0){
+            return res.status(400).json({
+                error:'Al menos una categoria es necesaria'
+            })
+        }
+        
+        if(!tags || tags.length == 0){
+            return res.status(400).json({
+                error:'Al menos una etiqueta es requerida'
+            })
+        }
+        
         let blog = new Blog();
         blog.title = title;
         blog.body = body;
@@ -26,6 +50,13 @@ exports.create = (req, res) => {
         blog.mtitle = `${title} | ${process.env.APP_NAME}`;
         blog.mdesc = stripHtml(body.substring(0,150));
         blog.postedBy = req.user._id;
+
+        // categories and tags
+
+        let arrayOfCategories = categories && categories.split(',')
+        let arrayOfTags = tags && tags.split(',')
+
+
 
         if(files.photo) {
             if(files.photo.size > 10000000) {
@@ -42,7 +73,24 @@ exports.create = (req, res) => {
                     error: errorHandler(err)
                 })
             }
-            res.json(result)
+           // res.json(result)
+            Blog.findByIdAndUpdate(result._id, {$push:{categories: arrayOfCategories }},{new: true}).exec((err,result) => {
+                if(err){
+                    return res.status(400).json({
+                        error: errorHandler(err)
+                    })
+                }  else{
+                    Blog.findByIdAndUpdate(result._id,{$push: {tags: arrayOfTags}},{new:true }).exec((err,result) => {
+                        if(err){
+                            return res.status(400).json({
+                                error: errorHandler(err)
+                            })
+                        } else {
+                            res.json(result)
+                        }
+                    })
+                }
+            })
         })
     })
 };
